14cd5cb87204c1de856248b88dfcc19adbb027e2
egeseckin <ege.seckin@outlook.com>
2021-10-22 12:02:17 +0300

    Initial Commit

---
+++ HW5_EgeSeckin/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW5_EgeSeckin
+//
+//  Created by Ege Seçkin on 22.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
